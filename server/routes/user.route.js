762a5da11186968d86dc677a5a61dbb155586dda
goodsie <mgoodie@outlook.com>
2025-11-13 11:47:24 -0500

    route correction

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,10 +4,10 @@ import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/api/users/', userController.createUser);
+router.post('/', userController.createUser);
 
-router.get('/api/users/:id', authMiddleware.requireSignin, userController.getUser);
-router.put('/api/users/:id', authMiddleware.requireSignin, authMiddleware.hasAuthorization, userController.updateUser);
-router.delete('/api/users/:id', authMiddleware.requireSignin, authMiddleware.hasAuthorization, userController.deleteUser);
+router.get('/:id', authMiddleware.requireSignin, userController.getUser);
+router.put('/:id', authMiddleware.requireSignin, authMiddleware.hasAuthorization, userController.updateUser);
+router.delete('/:id', authMiddleware.requireSignin, authMiddleware.hasAuthorization, userController.deleteUser);
 
 export default router;
